3a8cb895121c8895820722f7e7666adba9d96a25
turbostar190 <federicoc.2000@gmail.com>
2017-03-07 12:56:07 +0100

    Program uploads

---
+++ ram.py
@@ -0,0 +1,10 @@
+#https://github.com/turbostar190
+
+'''
+Try to run this simple program and watch RAM usage in task manager...
+Windows 7 should stop this.
+'''
+
+a = []
+while True:
+    a.append(0)
